3f31f92d70ec57e9285d25761e3179ab139023b1
Javier Vilchez <vilchezjavierf@gmail.com>
2024-09-06 22:29:10 -0300

    project configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/layout/layout.routes').then((m) => m.routes),
+  },
+  {
+    path: 'login',
+    title: 'Iniciar sesión',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'register',
+    title: 'Registrarse',
+    loadComponent: () =>
+      import('./pages/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+  },
+];
