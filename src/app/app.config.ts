128c34ba4e6a4038c1a5c3e2a7dfc8940edf1c70
Javier Vilchez <vilchezjavierf@gmail.com>
2024-09-10 22:55:21 -0300

    update code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,6 +15,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({"projectId":"sala-juegos-vilchez-javier","appId":"1:302591967328:web:e46a167cdfd5cb08d30305","storageBucket":"sala-juegos-vilchez-javier.appspot.com","apiKey":"AIzaSyCjkmME1czV1gcyXaz9ssb-gLlp2275jiI","authDomain":"sala-juegos-vilchez-javier.firebaseapp.com","messagingSenderId":"302591967328"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
+    provideFirestore(() => getFirestore()),
   ],
 };
